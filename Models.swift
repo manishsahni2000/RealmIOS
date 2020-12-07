0c8ca0c6b8b6959e7f09f0b87e42caa07c6febbd
Manish Sahni <manishsahni@Manishs-MacBook-Pro.local>
2020-12-07 14:55:39 +0530

    My First Realm IOS App

---
+++ Models.swift
@@ -0,0 +1,70 @@
+//
+//  Project.swift
+//  Task Tracker
+//
+//  Created by MongoDB on 2020-05-07.
+//  Copyright © 2020 MongoDB, Inc. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+typealias ProjectId = String
+
+class Project: Object {
+    @objc dynamic var _id: ObjectId = ObjectId.generate()
+    @objc dynamic var _partition: String = ""
+    @objc dynamic var name: String = ""
+    override static func primaryKey() -> String? {
+        return "_id"
+    }
+    
+    convenience init(partition: String, name: String) {
+        self.init()
+        self._partition = partition
+        self.name = name
+    }
+}
+
+class User: Object {
+    @objc dynamic var _id: ObjectId = ObjectId.generate()
+    @objc dynamic var _partition: String = ""
+    @objc dynamic var image: String? = nil
+    @objc dynamic var name: String = ""
+    override static func primaryKey() -> String? {
+        return "_id"
+    }
+}
+
+enum TaskStatus: String {
+  case Open
+  case InProgress
+  case Complete
+}
+
+class Task: Object {
+    @objc dynamic var _id: ObjectId = ObjectId.generate()
+    @objc dynamic var _partition: ProjectId = ""
+    @objc dynamic var assignee: User?
+    @objc dynamic var name = ""
+    @objc dynamic var status = TaskStatus.Open.rawValue
+
+    var statusEnum: TaskStatus {
+        get {
+            return TaskStatus(rawValue: status) ?? .Open
+        }
+        set {
+            status = newValue.rawValue
+        }
+    }
+
+    override static func primaryKey() -> String? {
+        return "_id"
+    }
+    
+    convenience init(partition: String, name: String) {
+        self.init()
+        self._partition = partition
+        self.name = name
+    }
+}
---
+++ WelcomeViewController.swift
@@ -0,0 +1,169 @@
+//
+//  WelcomeViewController.swift
+//  Task Tracker
+//
+//  Created by MongoDB on 2020-04-30.
+//  Copyright © 2020 MongoDB, Inc. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+// The WelcomeViewController handles login and account creation.
+class WelcomeViewController: UIViewController {
+    let emailField = UITextField()
+    let passwordField = UITextField()
+    let signInButton = UIButton(type: .roundedRect)
+    let signUpButton = UIButton(type: .roundedRect)
+    let errorLabel = UILabel()
+    let activityIndicator = UIActivityIndicatorView(style: .medium)
+
+    var email: String? {
+        get {
+            return emailField.text
+        }
+    }
+
+    var password: String? {
+        get {
+            return passwordField.text
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        view.backgroundColor = .white
+
+        // Create a view that will automatically lay out the other controls.
+        let container = UIStackView();
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.axis = .vertical
+        container.alignment = .fill
+        container.spacing = 16.0;
+        view.addSubview(container)
+
+        // Configure the activity indicator.
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+
+        // Set the layout constraints of the container view and the activity indicator.
+        let guide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            // This pins the container view to the top and stretches it to fill the parent
+            // view horizontally.
+            container.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 16),
+            container.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -16),
+            container.topAnchor.constraint(equalTo: guide.topAnchor, constant: 16),
+            // The activity indicator is centered over the rest of the view.
+            activityIndicator.centerYAnchor.constraint(equalTo: guide.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+            ])
+
+        // Add some text at the top of the view to explain what to do.
+        let infoLabel = UILabel()
+        infoLabel.numberOfLines = 0
+        infoLabel.text = "Please enter an email and password."
+        container.addArrangedSubview(infoLabel)
+
+        // Configure the email and password text input fields.
+        emailField.placeholder = "Email"
+        emailField.borderStyle = .roundedRect
+        emailField.autocapitalizationType = .none
+        emailField.autocorrectionType = .no
+        container.addArrangedSubview(emailField)
+
+        passwordField.placeholder = "Password"
+        passwordField.isSecureTextEntry = true
+        passwordField.borderStyle = .roundedRect
+        container.addArrangedSubview(passwordField)
+
+        // Configure the sign in and sign up buttons.
+        signInButton.setTitle("Sign In", for: .normal);
+        signInButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
+        container.addArrangedSubview(signInButton)
+
+        signUpButton.setTitle("Sign Up", for: .normal);
+        signUpButton.addTarget(self, action: #selector(signUp), for: .touchUpInside)
+        container.addArrangedSubview(signUpButton)
+
+        // Error messages will be set on the errorLabel.
+        errorLabel.numberOfLines = 0
+        errorLabel.textColor = .red
+        container.addArrangedSubview(errorLabel)
+    }
+
+    // Turn on or off the activity indicator.
+    func setLoading(_ loading: Bool) {
+        if loading {
+            activityIndicator.startAnimating();
+            errorLabel.text = "";
+        } else {
+            activityIndicator.stopAnimating();
+        }
+        emailField.isEnabled = !loading
+        passwordField.isEnabled = !loading
+        signInButton.isEnabled = !loading
+        signUpButton.isEnabled = !loading
+    }
+
+    @objc func signUp() {
+        setLoading(true);
+        app.emailPasswordAuth().registerUser(email: email!, password: password!) { [weak self](error) in
+            // Completion handlers are not necessarily called on the UI thread.
+            // This call to DispatchQueue.main.sync ensures that any changes to the UI,
+            // namely disabling the loading indicator and navigating to the next page,
+            // are handled on the UI thread:
+            DispatchQueue.main.sync {
+                self!.setLoading(false);
+                guard error == nil else {
+                    print("Signup failed: \(error!)")
+                    self!.errorLabel.text = "Signup failed: \(error!.localizedDescription)"
+                    return
+                }
+                print("Signup successful!")
+
+                // Registering just registers. Now we need to sign in, but we can reuse the existing email and password.
+                self!.errorLabel.text = "Signup successful! Signing in..."
+                self!.signIn()
+            }
+        }
+    }
+
+    @objc func signIn() {
+        print("Log in as user: \(email!)");
+        setLoading(true);
+
+        app.login(credentials: Credentials(email: email!, password: password!)) { [weak self](user, error) in
+            // Completion handlers are not necessarily called on the UI thread.
+            // This call to DispatchQueue.main.sync ensures that any changes to the UI,
+            // namely disabling the loading indicator and navigating to the next page,
+            // are handled on the UI thread:
+            DispatchQueue.main.sync {
+                self!.setLoading(false);
+                guard error == nil else {
+                    // Auth error: user already exists? Try logging in as that user.
+                    print("Login failed: \(error!)");
+                    self!.errorLabel.text = "Login failed: \(error!.localizedDescription)"
+                    return
+                }
+
+                print("Login succeeded!");
+
+                // Go directly to the Tasks page for the hardcoded project ID "My Project".
+                // This tutorial uses a common project to demonstrate sync.
+                let partitionValue = "My Project"
+                let realm = try! Realm()
+                print("Path: \(String(describing: realm.configuration.fileURL?.path))")
+
+                // Open a realm.
+                Realm.asyncOpen(configuration: user!.configuration(partitionValue: partitionValue)) { [weak self](realm, error) in
+                    guard let realm = realm else {
+                        fatalError("Failed to open realm: \(error!.localizedDescription)")
+                       
+                    }
+                    self!.navigationController!.pushViewController(TasksViewController(projectRealm: realm), animated: true);
+                }
+            }
+        };
+    }
+}
